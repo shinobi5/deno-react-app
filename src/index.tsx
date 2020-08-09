b76780b88b1434dfe3f2726441ac7b1d2a7cc7a4
shinobi5 <shinobi5@pm.me>
2020-08-09 20:55:22 +1200

    docs: add ts-ignores comment

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'https://cdn.skypack.dev/react-dom';
 import App from './containers/App/App.tsx';
 
 window.addEventListener('DOMContentLoaded', () => {
+  // @todo: get rid of these ts-ignores...
   // @ts-ignore
   ReactDOM.hydrate(
     //@ts-ignore
